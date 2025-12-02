59a3d14e6aa25d59c60b3d580f6ba15ae761f980
Adrian Fritz <adrian.fritz@it.htl-hl.ac.at>
2025-12-02 11:27:44 +0100

    SEW (we did a lot)

--- SEW/Projekte/CarConfiguratorFX/src/main/java/at/htlhl/carconfiguratorfx/CarController.java
+++ SEW/Projekte/CarConfiguratorFX/src/main/java/at/htlhl/carconfiguratorfx/CarController.java
@@ -1,5 +1,7 @@
 package at.htlhl.carconfiguratorfx;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
@@ -59,6 +61,13 @@ public class CarController {
         powerSlider.setMax(Car.MAX_POWER);
 
         assignModelDefaults();
+
+        manufacturerTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                System.out.println("Source: " + observableValue + ", oldValue: '" + oldValue + "', newValue: '" + newValue + "'");
+            }
+        });
     }
 
     private void assignModelDefaults() {
@@ -75,9 +84,34 @@ public class CarController {
         model.setRange((int) rangeSlider.getValue());
     }
 
+    private void updateViewFromModel() {
+        manufacturerTextField.setText(model.getManufacturer());
+        typeTextField.setText(model.getType());
+        powerSlider.setValue(model.getPower());
+        rangeSlider.setValue(model.getRange());
+    }
+
+    private void updateModel(Car newModel) {
+        model.setManufacturer(newModel.getManufacturer());
+        model.setType(newModel.getType());
+        model.setPower(newModel.getPower());
+        model.setRange(newModel.getRange());
+    }
+
     @FXML
     protected void loadAction(ActionEvent event) {
         System.out.println("Load clicked ... " + event.getSource());
+
+        File configFile = new File(App.MODEL_FILE_PATH);
+        if (configFile.exists()) {
+            try {
+                Car car = App.JSON_MAPPER.readValue(configFile, Car.class);
+                updateModel(car);
+                updateViewFromModel();
+            } catch (IOException ioex) {
+                System.err.println("Reading config file failed: " + ioex.getMessage());
+            }
+        }
     }
 
     @FXML
@@ -89,7 +123,7 @@ public class CarController {
         File configFile = new File(App.MODEL_FILE_PATH);
 
         try {
-            App.JSON_MAPPER.writer().writeValue(configFile, model);
+            App.JSON_MAPPER.writerWithDefaultPrettyPrinter().writeValue(configFile, model);
         } catch (IOException ex) {
             System.err.println("Writing config file failed: " + ex.getMessage());
         }
